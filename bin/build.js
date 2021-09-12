3efe09adf028d91a02c08bc5ca1533e46c2bafa4
Louis Pilfold <louis@lpil.uk>
2021-09-12 12:44:25 +0100

    Combine build script

--- bin/build.js
+++ bin/build.js
@@ -1,5 +1,5 @@
-import { copyFile, readFile, mkdir, access, readdir } from "fs/promises";
-import { join } from "path";
+import { stat, copyFile, readFile, mkdir, access, readdir } from "fs/promises";
+import { resolve, relative, join } from "path";
 import { promisify } from "util";
 import { exec as callbackExec } from "child_process";
 
@@ -46,7 +46,6 @@ async function buildProject({ name, root, dependencies, includeTests }) {
   let dir = root || libraryDir(name);
   let src = join(dir, "src");
   let test = join(dir, "test");
-  console.log("src", src, "test", test);
   let out = outDir(name);
   try {
     await exec(
@@ -90,3 +89,68 @@ async function fileExists(path) {
     return false;
   }
 }
+
+async function test() {
+  let gleamPackage = await build();
+
+  console.log("Running tests...");
+
+  let dir = `target/lib/${gleamPackage.name}`;
+  let passes = 0;
+  let failures = 0;
+
+  for await (let path of await getFiles(dir)) {
+    if (!path.endsWith("_test.js")) continue;
+    let module = await import(path);
+
+    for await (let fnName of Object.keys(module)) {
+      if (!fnName.endsWith("_test")) continue;
+      try {
+        await module[fnName]();
+        process.stdout.write(`\u001b[32m.\u001b[0m`);
+        passes++;
+      } catch (error) {
+        let moduleName = "\n" + relative(dir, path).slice(0, -3);
+        process.stdout.write(`\n❌ ${moduleName}.${fnName}: ${error}\n`);
+        failures++;
+      }
+    }
+  }
+
+  console.log(`
+
+${passes + failures} tests
+${failures} failures`);
+  process.exit(failures ? 1 : 0);
+}
+
+async function getFiles(dir) {
+  const subdirs = await readdir(dir);
+  const files = await Promise.all(
+    subdirs.map(async (subdir) => {
+      const res = resolve(dir, subdir);
+      return (await stat(res)).isDirectory() ? getFiles(res) : res;
+    })
+  );
+  return files.reduce((a, f) => a.concat(f), []);
+}
+
+async function main() {
+  switch (process.argv[process.argv.length - 1]) {
+    case "build":
+      return await build();
+
+    case "test":
+      return await test();
+
+    default:
+      console.error(`
+Usage: 
+  node bin/build.js test
+  node bin/build.js build
+`);
+      process.exit(1);
+  }
+}
+
+main();
